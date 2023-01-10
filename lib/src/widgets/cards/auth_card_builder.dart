b4180f5e6cd3e56256aec7cb3dd3649ae8e42a30
Christoph Stilling <c.stilling@91interactive.com>
2023-01-10 17:29:24 +0100

    delete rotation on login?

--- lib/src/widgets/cards/auth_card_builder.dart
+++ lib/src/widgets/cards/auth_card_builder.dart
@@ -466,7 +466,7 @@ class AuthCardState extends State<AuthCard> with TickerProviderStateMixin {
         return Transform(
           alignment: Alignment.center,
           transform: Matrix4.identity()
-            ..rotateZ(_cardRotationAnimation.value)
+            // ..rotateZ(_cardRotationAnimation.value)
             ..scale(_cardSizeAnimation.value, _cardSizeAnimation.value)
             ..scale(_cardSize2AnimationX.value, _cardSize2AnimationY.value),
           child: current,
